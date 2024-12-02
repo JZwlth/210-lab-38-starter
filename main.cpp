a6dbb870111cbcf23072fad76c86a50dd6eaf272
JZwlth <wkjazy87@gmail.com>
2024-12-01 20:43:24 -0800

    update

--- main.cpp
+++ main.cpp
@@ -14,10 +14,40 @@ int main() {
     cout << "\nMenu Options:" << endl;
     cout << "1. Add a code" << endl;
     cout << "2. Delete a code" << endl;
-    cout << "3. S
+    cout << "3. S;
     cout << "Enter your choice: ";
 }
 
+void processChoice(IntBinaryTree &tree) {
+    int choice;
+    string code;
+    string newCode;
+
+    do {
+        displayMenu();
+        cin >> choice;
+        cin.ignore(); // Ignore newline character after integer input
+
+        switch (choice) {
+            case 1:
+                cout << "Enter code to add: ";
+                getline(cin, code);
+                tree.insertNode(code);
+                cout << "Code added." << endl;
+                break;
+            case 2:
+                cout << "Enter code to delete: ";
+                getline(cin, code);
+                if (tree.searchNode(code)) {
+                    tree.remove(code);
+                    cout << "Code deleted." << endl;
+                } else {
+                    cout << "Code not found." << endl;
+                }
+                break;
+            case 3:
+                cout << "Enter code to search: ";
+                getline(cin, code);
 
     return 0;
 };
